0991e8d6c364e45cf4942d2deaee70a221eacbc4
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-06-16 21:06:53 -0500

    added basic style

--- src/components/scoreboard.js
+++ src/components/scoreboard.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "../style/scoreboard.css";
 
 const ScoreBoard = ({ currentScore, bestScore }) => {
   return (
-    <>
+    <div className="scores">
       <div id="currentScore">Current Score: {currentScore}</div>
       <div id="bestScore">Best Score: {bestScore}</div>
-    </>
+    </div>
   );
 };
 
